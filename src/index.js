6d7a706dacaaaa4d089ccc7f5889e5e68dfd5d3e
tony-tripulca <tony.tripulca@gmail.com>
2023-05-03 06:55:26 +0800

    Changed to movie api

--- src/index.js
+++ src/index.js
@@ -1,81 +1,33 @@
-function getLatest() {
-  let domain = "https://newsapi.org";
-  let endpoint = "/v2/top-headlines";
-  let query = "country=ph";
+function searchMovie() {
+  let domain = "http://www.omdbapi.com";
   let results = [];
+
+  let movieTitle = document.querySelector(".movie-title-input").value;
+  let query = `apikey=${app.apiKey}&s=${movieTitle}`;
 
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      results = JSON.parse(xhttp.responseText).articles;
+      let results = JSON.parse(xhttp.responseText).Search;
 
-      let newsRow = document.querySelector(".news-row");
+      let newsRow = document.querySelector(".movies-row");
       results.map((result, key) => {
-        newsRow.appendChild(document.createElement("div.col")).innerHTML = `
-        <div class="card mb-3">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src="${ result.urlToImage || "./src/assets/default_news.jpg"
-              }" class="img-fluid rounded-start" alt="..." />
-            </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <h5 class="card-title">${result.title}</h5>
-                <p class="card-text">${result.description || ""}</p>
-                <p class="card-text">
-                  <small class="text-body-secondary"
-                    >${result.publishedAt.substring(0, 10)} - ${ result.author
-                    }</small
-                  >
-                </p>
-                <button
-                  type="button"
-                  class="btn btn-secondary mb-3"
-                  data-bs-toggle="modal"
-                  data-bs-target="#news-${key}"
-                >
-                  Contents
-                </button>
-                <p><a href="${result.url}" target="_blank">View source</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="modal fade" id="news-${key}" tabindex="-1">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h1 class="modal-title fs-5">${result.title}</h1>
-                <button
-                  type="button"
-                  class="btn-close"
-                  data-bs-dismiss="modal"
-                ></button>
-              </div>
-              <div class="modal-body">
-                <p>${result.content || "No Contents"}</p>
-                <p><a href="${result.url}" target="_blank">Read more...</a></p>
-              </div>
-              <div class="modal-footer">
-                <button
-                  type="button"
-                  class="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        let element = document.createElement("div");
+        element.classList.add("col-12");
+        element.classList.add("col-md-3");
+        element.innerHTML = `
+        <a href="./movie.html?id=${result.imdbID}" alt="${result.Title}">
+          <img src="${result.Poster || "./src/assets/movies.jpg"}"
+          class="img-fluid rounded" alt="${result.Title}" />
+        </a>
         `;
+        newsRow.appendChild(element);
       });
     }
   };
 
-  xhttp.open("GET", `${domain}/${endpoint}?${query}`, true);
-  xhttp.setRequestHeader("X-Api-Key", app.apiKey);
+  xhttp.open("GET", `${domain}?${query}`, true);
   xhttp.send();
 }
 
@@ -91,7 +43,6 @@ function getDate() {
 
 function init() {
   getDate();
-  getLatest();
 }
 
 init();
---
+++ src/movie.js
@@ -0,0 +1,83 @@
+function getMovieDetails() {
+  let url = new URL(location.href);
+  let params = new URLSearchParams(url.search);
+
+  let domain = "http://www.omdbapi.com";
+  let results = [];
+
+  let query = `apikey=${app.apiKey}&i=${params.get("id")}`;
+
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(JSON.parse(xhttp.responseText));
+      let res = JSON.parse(xhttp.responseText);
+
+      let newsRow = document.querySelector(".movie-details");
+      let element = document.createElement("div");
+      element.classList.add("col-12");
+      element.innerHTML = `<div class="card mb-3">
+      <div class="row g-0">
+        <div class="col-md-3">
+          <img src="${res.Poster || "./src/assets/movies.jpg"}"
+          class="img-fluid rounded-start" alt="..." />
+        </div>
+        <div class="col-md-9">
+          <div class="card-body">
+            <h5 class="card-title fs-2">
+              ${res.Title}
+              <span class="badge bg-primary fs-6">${res.imdbRating}</span>
+            </h5>
+            <div class="row">
+              <div class="col-12 mb-3">
+                <p class="card-text">${res.Plot}</p>
+              </div>
+              <div class="col-12 col-md-3 mb-3">
+                <p class="card-text fw-bold">ACTORS</p>
+                <p class="card-text">${res.Actors}</p>
+              </div>
+              <div class="col-12 col-md-3 mb-3">
+                <p class="card-text fw-bold">DIRECTOR</p>
+                <p class="card-text">${res.Director}</p>
+              </div>
+              <div class="col-12 col-md-3 mb-3">
+                <p class="card-text fw-bold">COUNTRY</p>
+                <p class="card-text">${res.Country}</p>
+              </div>
+              <div class="col-12 col-md-3 mb-3">
+                <p class="card-text fw-bold">RELEASED</p>
+                <p class="card-text">${res.Released}</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12 col-md-3 mb-3">
+                <p class="card-text fw-bold">GENRE</p>
+                <p class="card-text">${res.Genre}</p>
+              </div>
+              <div class="col-12 col-md-3 mb-3">
+                <p class="card-text fw-bold">LANGUAGE</p>
+                <p class="card-text">${res.Language}</p>
+              </div>
+              <div class="col-12 col-md-3 mb-3">
+                <p class="card-text fw-bold">RUNTIME</p>
+                <p class="card-text">${res.Runtime}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+      newsRow.appendChild(element);
+    }
+  };
+
+  xhttp.open("GET", `${domain}?${query}`, true);
+  xhttp.send();
+}
+
+function init() {
+  getMovieDetails();
+}
+
+init();
